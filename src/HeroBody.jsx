1b90a5b445897ce1948691cfeaf2c089c4ce7ada
tirumalateja19 <tirumalateja.jampani@gmail.com>
2025-11-29 14:40:26 +0530

    routing

---
+++ src/HeroBody.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./NavBar";
+import Footer from "./Footer.jsx";
+import { Outlet } from "react-router-dom";
+
+const HeroBody = () => {
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default HeroBody;
